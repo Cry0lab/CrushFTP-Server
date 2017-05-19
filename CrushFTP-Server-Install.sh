a04890ac4a4c021a8483f953794bc45d6471208e
YankeeForty2 <daniel.molina@email.swbts.edu>
2017-05-19 11:48:38 -0500

    Update CrushFTP-Server-Install.sh

--- CrushFTP-Server-Install.sh
+++ CrushFTP-Server-Install.sh
@@ -69,13 +69,13 @@ validIP()
 I=$IP
 
 if [ "$(ipcalc -cs $I && echo 1 || echo 0)" == 0 ]; then
-echo "Please enter a valid IP Address:"
+echo "That is not a valid IP Address. Please enter a valid IP Address:"
 read IP
 validIP
 return 0
 fi
 
-if [ "$(isAlive $I)" == "1" ]; then
+if [ "$(isAlive $I)" == "1" ] && [ "$I" != "$DHCP_IP" ]; then
  echo "That IP ADDRESS is already in use. Please enter a different one:"
  read IP
  validIP
